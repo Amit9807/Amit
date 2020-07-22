f3a9aef664982841cac759b633e33caa3f625a68
Amit9807 <samit9076@gmail.com>
2020-07-22 14:23:17 +0530

    v2

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h2>Personal Portfolio</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ portfolio/src/components/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class About extends Component{
+  render(){
+    return(
+      <div>
+        <h2>About Page</h2>
+      </div>
+    )
+  }
+}
+
+export default About;
